f574b3eb97452864b92419aa5bdbc97a67483625
Alexander Groden <alexander.groden@gmail.com>
2014-02-21 17:36:08 -0500

    prepping to use glfw source

--- ext/ext.mk
+++ ext/ext.mk
@@ -5,6 +5,7 @@ GLEW_DIR:=$(EXT_DIR)/glew
 GLFW_DIR:=$(EXT_DIR)/glfw
 GLM_DIR:=$(EXT_DIR)/glm
 
+
 ################################################################################
 # file lists
 ################################################################################
@@ -41,8 +42,24 @@ $(GLEW_DIR)/lib/$(GLEW_LIB): $(GLEW_DIR)/src/glew.c
 	echo $@
 	cd $(GLEW_DIR); make
 
+# glfw config makefile
+$(GLFW_DIR)/Makefile:
+	cd $(GLFW_LIB) && cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/home/agroden/code/glfw/lib -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_INSTALL=OFF
+
+# glfw lib
+$(GLFW_DIR)/lib/$(GLFW_LIB): $(GLFW_DIR)/Makefile
+	@mkdir -p $(GLFW_DIR)/lib
+	cd $(GLFW_DIR) && \
+		make && \
+		$(CP) $(GLFW_DIR)/src/$(GLFW_LIB) $(GLFW_DIR)/lib/$(GLFW_DIR)/lib
+
 clean_glew:
 	cd $(GLEW_DIR)/auto && make destroy
 	cd $(GLEW_DIR) && make clean
 
-clean_ext: clean_glew
+clean_glfw:
+	cd $(GLFW_DIR) && make clean
+	$(RM) -rf $(GLFW_DIR)/*.cmake
+	$(RM) -rf $(GLFW_DIR)/CMakeFiles
+
+clean_ext: clean_glew clean_glfw
